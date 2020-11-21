9c48b370a5bc94c11be85adbab4ee245f973c6c9
johnkim0306 <mkcokr2176@gmail.com>
2020-11-21 09:52:13 -0700

    Updates

---
+++ precache-manifest.c6bcd604af6491cd1e06c6e0b103d814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd08b6fa0829a057a742c5935820d5cc",
+    "url": "/johnkim0306.Covid19/index.html"
+  },
+  {
+    "revision": "7fc88234d01fd87aa18d",
+    "url": "/johnkim0306.Covid19/static/css/main.966c2c1f.chunk.css"
+  },
+  {
+    "revision": "a7d0773c8d91a5d928ec",
+    "url": "/johnkim0306.Covid19/static/js/2.8c8eff29.chunk.js"
+  },
+  {
+    "revision": "0aaa08c1dd43d3bb7a3f72b856eccd33",
+    "url": "/johnkim0306.Covid19/static/js/2.8c8eff29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc88234d01fd87aa18d",
+    "url": "/johnkim0306.Covid19/static/js/main.ec5582e9.chunk.js"
+  },
+  {
+    "revision": "ce841fb17a96a98cafa6",
+    "url": "/johnkim0306.Covid19/static/js/runtime-main.286a91e8.js"
+  }
+]);
